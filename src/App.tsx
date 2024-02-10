4f3353e0d97e421a2e757bcd2d12abe2eeadebcb
Sarun Kokpol <sarun_k@tripetch-it.co.th>
2024-02-10 13:26:27 +0700

    complete text for tablet

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 import ItemHeading from './components/ItemHeading';
 import athletesImageDesktop from './assets/athletes-desktop.png';
 import playerImageDesktop from './assets/players-desktop.png';
+// import athletesImageTablet from '/assets/athletes-tablet.png';
+// import playerImageTablet from '/assets/players-tablet.png';
+
+const QUERIES = {
+  tabletAndSmaller: '(max-width: 1300px)',
+  mobileAndSmaller: '(max-width: 768px)',
+};
 
 const Section = styled.section`
   position: relative;
@@ -13,6 +20,10 @@ const AthletesImage = styled.img`
   left: 9.11%;
   max-width: 42%;
   max-height: calc(100% + 20px);
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const PlayersImage = styled.img`
@@ -21,6 +32,10 @@ const PlayersImage = styled.img`
   right: 6.51%;
   max-width: 51.6%;
   max-height: 93.5%;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const Heading = styled.h1<{ side: 'left' | 'right' }>`
@@ -31,21 +46,39 @@ const Heading = styled.h1<{ side: 'left' | 'right' }>`
   padding-top: 100px;
   padding-left: ${(props) => (props.side === 'left' ? '16.77%' : '52.5%')};
   margin-bottom: ${(props) => (props.side === 'left' ? '20px' : '10px')};
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    padding-top: 50px;
+    padding-left: ${(props) => (props.side === 'left' ? '30px' : '37.89%')};
+    margin-bottom: 15px;
+  }
 `;
 
 const Item = styled.li<{ backgroundColor?: string; side: 'left' | 'right' }>`
   background-color: ${(props) => props.backgroundColor || '#ffffff'};
   padding: 60px 0;
   padding-left: ${(props) => (props.side === 'left' ? '16.77%' : '52.5%')};
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    padding: 30px 0;
+    padding-left: ${(props) => (props.side === 'left' ? '30px' : '37.89%')};
+  }
 `;
 
-const Text = styled.p<{ color?: 'white' }>`
+const Text = styled.p<{ color?: 'white'; side: 'left' | 'right' }>`
   color: ${(props) => props.color || 'black'};
   font-size: 20px;
   font-weight: 400;
   line-height: 28px;
   max-width: 717px;
-  margin-right: 48px;
+  margin-right: 30px;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    font-size: 18px;
+    letter-spacing: 0px;
+
+    max-width: ${(props) => (props.side === 'left' ? '58.2%' : 'none')};
+  }
 `;
 
 function App() {
@@ -56,14 +89,14 @@ function App() {
         <ol>
           <Item side="right">
             <ItemHeading number="01">CONNECTION</ItemHeading>
-            <Text>
+            <Text side="right">
               Connect with coaches directly, you can ping coaches to view
               profile.
             </Text>
           </Item>
           <Item backgroundColor="#f5f4f9" side="right">
             <ItemHeading number="02">COLLABORATION</ItemHeading>
-            <Text>
+            <Text side="right">
               Work with other student athletes to increase visability. When you
               share and like other players videos it will increase your
               visability as a player. This is the team work aspect to Surface 1.
@@ -73,7 +106,7 @@ function App() {
             <ItemHeading number="03" numberUnderlineColor="white">
               GROWTH
             </ItemHeading>
-            <Text color="white">
+            <Text color="white" side="right">
               Resources and tools for you to get better as a student Athelte.
               Access to training classes, tutor sessions, etc
             </Text>
@@ -86,14 +119,14 @@ function App() {
         <ol>
           <Item side="left">
             <ItemHeading number="01">CONNECTION</ItemHeading>
-            <Text>
+            <Text side="left">
               Connect with talented athlete directly, you can watch their skills
               through video showreels directly from Surface 1.
             </Text>
           </Item>
           <Item backgroundColor="#f5f4f9" side="left">
             <ItemHeading number="02">COLLABORATION</ItemHeading>
-            <Text>
+            <Text side="left">
               Work with recruiter to increase your chances of finding talented
               athlete.
             </Text>
@@ -106,7 +139,7 @@ function App() {
             >
               GROWTH
             </ItemHeading>
-            <Text color="white">
+            <Text color="white" side="left">
               Save your time, recruit proper athlets for your team.
             </Text>
           </Item>
